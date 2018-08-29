10add0310efe3489e585d1c810c981bcc2d0f36a
Kei Fujikawa <fujikei22funk@gmail.com>
2018-08-30 00:06:58 +0900

    second commit

--- ARKitStudy/ViewController.swift
+++ ARKitStudy/ViewController.swift
@@ -10,71 +10,163 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
+class ViewController: UIViewController {
+    @IBOutlet weak var zNearLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureForDouble(_:)))
+        sceneView.addGestureRecognizer(gesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
+        
+        guard let camera = sceneView.pointOfView?.camera else { return }
+        let zNear = camera.zNear
+        zNearLabel.text = "zNear:" + (round(zNear * 1000) / 1000).description
+        
+        slider.value = 0
+        
+        print("zNear: \(camera.zNear)")
+        print("zFar: \(camera.zFar)")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    @objc func tapGestureWrong(_ recognizer: UITapGestureRecognizer) {
+        let finger = recognizer.location(in: nil)
+        airPlane.position = SCNVector3(finger.x, finger.y, 0.3)
+        sceneView.scene.rootNode.addChildNode(airPlane)
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
+    @objc func tapGesture(_ recognizer: UITapGestureRecognizer) {
+        // カメラ座標系で30cm前
+        let infrontOfCamera = SCNVector3(x: 0, y: 0, z: -0.3)
+        
+        // カメラ座標系 -> ワールド座標系
+        guard let cameraNode = sceneView.pointOfView else { return }
+        print(cameraNode.camera!.zNear)
+        print(cameraNode.camera!.zFar)
+        let pointInWorld = cameraNode.convertPosition(infrontOfCamera, to: nil)
+        
+        // ワールド座標系 -> スクリーン座標系
+        var screenPos = sceneView.projectPoint(pointInWorld)
+        
+        // スクリーン座標系で
+        // x, yだけ指の位置に変更
+        // zは変えない
+        let finger = recognizer.location(in: nil)
+        screenPos.x = Float(finger.x)
+        screenPos.y = Float(finger.y)
+        
+        // ワールド座標に戻す
+        let finalPosition = sceneView.unprojectPoint(screenPos)
+        
+        // nodeを置く
+        let airPlaneNode = airPlane
+        airPlaneNode.position = finalPosition
+        sceneView.scene.rootNode.addChildNode(airPlaneNode)
+    }
+    
+    @objc func tapGesture2(_ recognizer: UITapGestureRecognizer) {
+        // スクリーン座標系
+        let finger = recognizer.location(in: nil)
+        let pos = SCNVector3(finger.x, finger.y, 0.996)
+        
+        // ワールド座標に戻す
+        let finalPosition = sceneView.unprojectPoint(pos)
+        
+        // nodeを置く
+        let airPlaneNode = airPlane
+        airPlaneNode.position = finalPosition
+        sceneView.scene.rootNode.addChildNode(airPlaneNode)
+    }
+    
+    @objc func tapGestureForDouble(_ recognizer: UITapGestureRecognizer) {
+        // スクリーン座標系
+        let finger = recognizer.location(in: nil)
+        let pos = SCNVector3(finger.x, finger.y, 0.996)
+        
+        // ワールド座標に戻す
+        let finalPosition = sceneView.unprojectPoint(pos)
+        
+        // nodeを置く
+        let airPlaneNode = doubleAirPlanes
+        airPlaneNode.position = finalPosition
+        sceneView.scene.rootNode.addChildNode(airPlaneNode)
+    }
+    
+    private let airPlane: SCNNode = {
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let node = scene.rootNode
+        node.scale = SCNVector3(0.2, 0.2, 0.2)
         return node
-    }
-*/
+    }()
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    private let doubleAirPlanes: SCNNode = {
+        let scene = SCNScene(named: "art.scnassets/double.scn")!
+        let node = scene.rootNode
         
+        // 拡大
+        let ship1 = node.childNode(withName: "ship1", recursively: true)!
+        ship1.scale = SCNVector3(0.4, 0.4, 0.4) // 今回は0.2がデフォ
+        
+        // 回転 using SCNVector4
+        let ship2 = node.childNode(withName: "ship2", recursively: true)!
+        ship2.eulerAngles.x = -.pi / 4
+        
+        ship2.rotation = SCNVector4(x: 1,
+                                    y: 0,
+                                    z: 0,
+                                    w: -.pi / 4)
+        
+        
+        
+        
+        
+         ship2.rotation = SCNVector4(x: 0,
+                                    y: 1,
+                                    z: 0,
+                                    w: -.pi / 4)
+        // ship2.rotation = SCNVector4(x: 1, y: 0, z: 0, w: -.pi / 4)
+        // ship2.orientation = SCNQuaternion(x: <#T##Float#>, y: <#T##Float#>, z: <#T##Float#>, w: <#T##Float#>)
+        
+        return node
+    }()
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let value = sender.value
+        
+        if let camera = sceneView.pointOfView?.camera {
+            guard value > 0 else {
+                return
+            }
+            camera.zNear = Double(value * 1)
+            
+            let zNear = camera.zNear
+            zNearLabel.text = "zNear:" + (round(zNear * 1000) / 1000).description
+        }
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let cameraNode = sceneView.pointOfView else { return }
+        print(cameraNode.camera!.zNear)
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
