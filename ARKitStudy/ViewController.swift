9d926dd52c4b40d41c9b50ecf420f9f861326e20
Kei Fujikawa <fujikei22funk@gmail.com>
2018-10-15 19:48:46 +0900

    show ball

--- ARKitStudy/ViewController.swift
+++ ARKitStudy/ViewController.swift
@@ -19,9 +19,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         sceneView.delegate = self
-        
-        let gesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureForDouble(_:)))
-        sceneView.addGestureRecognizer(gesture)
+        sceneView.scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let ship = sceneView.scene.rootNode.childNode(withName: "ship", recursively: true)!
+        ship.transform = SCNMatrix4MakeRotation(.pi/2, 1, 0, 0)
+        ship.rotation = SCNVector4Make(1, 0, 0, .pi/2)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,6 +47,65 @@ class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        guard let camera = sceneView.pointOfView else { return }
+        let cameraPosition = SCNVector3(0, 0, -10)
+        let targetPosition = camera.convertPosition(cameraPosition, to: nil)
+        let startPos: SCNVector3 = camera.convertPosition(SCNVector3Make(0, 0, -0.1), to: nil)
+        showBubble(by: startPos, to: targetPosition)
+    }
+    
+    private func showBubble(by: SCNVector3, to: SCNVector3){
+        let node = getBallNode()
+        node.position = by
+        
+        // シャボン玉が飛んでいくアニメーション
+        let move = SCNAction.move(to: to, duration: 4.0)
+        move.timingMode = .easeIn
+        node.runAction(move, completionHandler: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                node.removeFromParentNode()
+            }
+        })
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    // ここでmaterial等を変更
+    private func getBallNode() -> BallNode {
+        switch self.segment {
+        case 0:
+            let node = BallNode()
+            node.setColor(color: .red)
+            return node
+        case 1:
+            let node = BallNode()
+            node.setColor(color: .blue)
+            return node
+        case 2:
+            let node = BallNode()
+            node.setColor(color: .yellow)
+            return node
+        case 3:
+            let node = BallNode()
+            node.setColor(color: .green)
+            return node
+        case 4:
+            let node = BallNode()
+            node.setColor(color: .cyan)
+            return node
+        default:
+            return BallNode()
+        }
+    }
+    
+    private var segment: Int = 0
+    
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        self.segment = sender.selectedSegmentIndex
+    }
+    
     @objc func tapGestureWrong(_ recognizer: UITapGestureRecognizer) {
         let finger = recognizer.location(in: nil)
         airPlane.position = SCNVector3(finger.x, finger.y, 0.3)
@@ -58,10 +118,7 @@ class ViewController: UIViewController {
         
         // カメラ座標系 -> ワールド座標系
         guard let cameraNode = sceneView.pointOfView else { return }
-        print(cameraNode.camera!.zNear)
-        print(cameraNode.camera!.zFar)
         let pointInWorld = cameraNode.convertPosition(infrontOfCamera, to: nil)
-        
         // ワールド座標系 -> スクリーン座標系
         var screenPos = sceneView.projectPoint(pointInWorld)
         
@@ -166,7 +223,7 @@ extension ViewController: ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         guard let cameraNode = sceneView.pointOfView else { return }
-        print(cameraNode.camera!.zNear)
+        print(cameraNode.camera!.focalLength)
     }
     
 }
